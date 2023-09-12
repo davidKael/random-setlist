1c451817dafc26b0aeab9b12b1c317820d2fbafc
davidKael <70914882+davidKael@users.noreply.github.com>
2023-09-12 17:53:49 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { allSongs } from "./Songs";
+import { useState } from "react";
+import logo from "./imgs/2022_Causeries_Vit_utan_bakgrund.jpg.png";
 
 function App() {
+  const [currentSetlist, setCurrentSetlist] = useState([]);
+
+  function handleRandomizeSetlist() {
+    setCurrentSetlist(shuffleArray(allSongs));
+  }
+
+  function shuffleArray(array) {
+    let shuffledArray = [...array];
+    for (var i = shuffledArray.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = shuffledArray[i];
+      shuffledArray[i] = shuffledArray[j];
+      shuffledArray[j] = temp;
+    }
+
+    return shuffledArray;
+  }
+
+  const renderedSetlist = currentSetlist.map((song, index) => {
+    return (
+      <div className="song-row" key={index + 1}>
+        {index + 1}: {song}
+      </div>
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <img src={logo} className="logo-img" />
+        {renderedSetlist.length > 0 && <h1>Todays Setlist:</h1>}
+      </div>
+      <div className="set-list">
+        {renderedSetlist.length > 0 && renderedSetlist}
+      </div>
+      <button onClick={handleRandomizeSetlist} className="random-btn">
+        Randomize Setlist
+      </button>
     </div>
   );
 }
---
+++ src/Songs.js
@@ -0,0 +1,12 @@
+export const allSongs = [
+  "All Time High",
+  "Zebra",
+  "Don't Be One of Us",
+  "Gen Y",
+  "Unisex Guns",
+  "Shitty Things",
+  "Nothing",
+  "EKG",
+  "Wicked",
+  "Let Go",
+];
